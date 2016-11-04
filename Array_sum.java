d9b4f4ef306c53255b944a8c7ee17d67fa8e9bad
Olena <oh-i-lenka@gavatyuk.org.ua>
2016-11-04 18:06:29 +0100

    added double

--- Array_sum.java
+++ Array_sum.java
@@ -5,22 +5,20 @@ import javax.xml.soap.SAAJMetaFactory;
  */
 public class Array_sum {
     public static void main(String[] args) {
-        /*int[] array = new int[10];
-        array [0] = 1;
-        array [1] = 200;*/
-        int[] arrayInt={79, 79, 79, -1, 1, 1, -1, 701, -6, 79};
+        int[] arrayInt={978, -7, 27, -7, 47, 97, 72, 71, 70, -7};
         double[] arrayDouble= {1,2,89, 580, -85, 900, -9000, 0, 78, -789};
-        /*int[] arrayInt = (int[]) arrayDouble;*/
-       /* double[] arrayDouble = (double[]) array;*/
-        /*sum (arrayInt);
-        sumDoouble(arrayDouble);
+       /* int[] arrayInt = (int[]) arrayDouble;*/
+        /*double[] arrayDouble = (double[]) arrayInt;*/
+        sum (arrayInt);
+        sum(arrayDouble);
         minElement(arrayInt);
         maxElement(arrayInt);
         maxPositiv(arrayInt);
         multiplication(arrayInt);
-        modulus(arrayInt);*/
-        secondLargest(arrayInt);
+        modulus(arrayInt);
+        secondLargest(arrayDouble);
     }
+
     static void sum (int array[]){
         int sumAll=0;
         for (int i=0; i<=9; i++){
@@ -31,7 +29,7 @@ public class Array_sum {
 
     }
 
-    static void sumDoouble (double array[]){
+    static void sum (double array[]){
         double sumAll=0;
         for (int i=0; i<=9; i++){
             sumAll+= array[i];
@@ -42,6 +40,17 @@ public class Array_sum {
 
     static void minElement (int array[]){
         int min = array[0];
+        for (int i=0; i<=8; i++){
+            if ((array[i]>array[i+1]) && (min > array[i+1])){
+                min = array[i+1];
+            }
+        }
+        System.out.println(min);
+
+    }
+
+    static void minElement (double array[]){
+        double min = array[0];
         for (int i=0; i<=8; i++){
             if ((array[i]>array[i+1]) && (min > array[i+1])){
                 min = array[i+1];
@@ -62,10 +71,36 @@ public class Array_sum {
 
     }
 
+    static void maxElement (double array[]){
+        double max = array[0];
+        for (int i=0; i<=8; i++){
+            if ((array[i]<array[i+1]) && (max < array[i+1])){
+                max = array[i+1];
+            }
+        }
+        System.out.println(max);
+
+    }
+
     static void maxPositiv (int array[]){
         int maxPos = 0;
         for (int i=0; i<=9; i++){
-            if (((array[i])>0) && (maxPos < array[i])){
+            if ((array[i]>0) && (maxPos < array[i])){
+                maxPos = array[i];
+            }
+        }
+        if (maxPos == 0){
+            System.out.println("There are no any positive elements in array");
+        } else {
+            System.out.println(maxPos);
+        }
+
+    }
+
+    static void maxPositiv (double array[]){
+        double maxPos = 0;
+        for (int i=0; i<=9; i++){
+            if ((array[i]>0) && (maxPos < array[i])){
                 maxPos = array[i];
             }
         }
@@ -86,8 +121,23 @@ public class Array_sum {
 
     }
 
+    static void multiplication (double array[]){
+        double multi = array[0];
+        for (int i=1; i<=9; i++){
+            multi*= array[i];
+        }
+        System.out.println(multi);
+
+    }
+
     static void modulus (int array[]){
         int mod = array[9] % array[0];
+        System.out.println(mod);
+
+    }
+
+    static void modulus (double array[]){
+        double mod = array[9] % array[0];
         System.out.println(mod);
 
     }
@@ -96,20 +146,42 @@ public class Array_sum {
         int max, secMax;
         max = secMax = array [0];
         int i = 1;
-        while (max == secMax) {
+        while ((max == secMax) && (i <= 9)) {
             if (array[i] != max){
                 secMax = array[i];
             }
             i++;
         }
-        /*if (array[0]>array[1]){
-            max = array[0];
-            secMax = array [1];
-        } else {
-            max = array[1];
-            secMax = array [0];
-        }*/
-        for (i=2; i<=9; i++){
+
+        for (i=1; i<=9; i++){
+            if (max < array[i]){
+                secMax = max;
+                max = array[i];
+            } else {
+                if ((secMax < array[i]) && (array[i] != max)) {
+                    secMax = array[i];
+                }
+            }
+        }
+        if (max == secMax) {
+            System.out.println("All elements in array are equal");
+        } else System.out.println(secMax);
+
+
+    }
+
+    static void secondLargest (double array[]){
+        double max, secMax;
+        max = secMax = array [0];
+        int i = 1;
+        while ((max == secMax) && (i <= 9)) {
+            if (array[i] != max){
+                secMax = array[i];
+            }
+            i++;
+        }
+
+        for (i=1; i<=9; i++){
             if (max < array[i]){
                 secMax = max;
                 max = array[i];
